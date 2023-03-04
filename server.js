fc964be6cefea6a9c95fb81b17e431ea7eaa3595
Noah berg <noah.berg@smartgroupsoftware.com>
2023-03-04 13:59:15 -0500

    updated files

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const path = require('path');
-const api = require('./Develop/routes');
+const api = require('./routes');
 
 const PORT = process.env.PORT || 3001;
 
