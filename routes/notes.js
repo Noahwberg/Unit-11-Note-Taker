01a0ed947054362d01f7638dd4ce59f0f8acfba4
Noah berg <noah.berg@smartgroupsoftware.com>
2023-03-04 13:41:15 -0500

    finish note taker

---
+++ routes/notes.js
@@ -0,0 +1,36 @@
+const notes = require('express').Router();
+const { v4: uuidv4 } = require('uuid');
+const { readFromFile, readAndAppend, writeToFile } = require('../utils/utils');
+
+notes.get('/', (req, res) => {
+    readFromFile('./db/db.json').then((data) => res.json(JSON.parse(data)));
+});
+
+notes.delete('/:id', (req, res) => {
+  const noteId = req.params.id;
+  readFromFile('./db/db.json')
+    .then((data) => JSON.parse(data))
+    .then((json) => {
+      const result = json.filter((note) => note.id !== noteId);
+
+      writeToFile('./db/db.json', result);
+      res.json(`Goodbye`);
+    });
+});
+
+notes.post('/', (req, res) => {
+  console.log(req.body);
+  const { title, text } = req.body;
+    if (req.body) {
+      const newNote = {
+        title, text, id: uuidv4(),
+      };
+  
+      readAndAppend(newNote, './db/db.json');
+      res.json(`Note added`);
+    } else {
+      res.error('Error in adding note');
+    }
+});
+
+module.exports = notes;
